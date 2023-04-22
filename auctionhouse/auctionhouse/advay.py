5ffddd29a70fdefca34105adea5b15cd60c28f9f
Advay Patil <advaypatil27@gmail.com>
2023-04-21 21:27:03 -0700

    idk what im doing

--- auctionhouse/auctionhouse/advay.py
+++ auctionhouse/auctionhouse/advay.py
@@ -38,20 +38,27 @@ def logistic_function(x):
 
 
 def ElDesafio(wallet, history):
+
+    if len(history) == 0:
+        return 0
+
     if history[-1] == 0 and len(history) < 5:
         return 1
 
     sum_of_wallet = sum(list(map(lambda x: x[0], history)))
-    valid_amount = int(sum_of_wallet * 0.50)
+    valid_amount = sum_of_wallet * 0.50
 
     return 0 if valid_amount > wallet else valid_amount + 1
 
 
 def ElDesafisimo(wallet, history):
+    if len(history) == 0:
+        return 0
+
     if history[-1] == 0 and len(history) < 5:
         return 1
 
     sum_of_wallet = sum(list(map(lambda x: x[0], history)))
-    valid_amount = int(sum_of_wallet * logistic_function(len(history)))
+    valid_amount = sum_of_wallet * logistic_function(len(history))
 
     return 1 if valid_amount > wallet else valid_amount + 1
