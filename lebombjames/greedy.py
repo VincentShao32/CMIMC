9f5b58c5374424bd959939ee862a68fdbc9db087
Yanda Bao <yandabaodav@gmail.com>
2023-04-22 09:28:54 -0700

    lebomb

--- lebombjames/greedy.py
+++ lebombjames/greedy.py
@@ -1,8 +1,3 @@
-"""
-Edit this file! This is the file you will submit.
-"""
-import random
-
 totalBoard = []
 currBoard = []
 centres = []
@@ -81,6 +76,7 @@ def calcCentres(x, y):
 
     return total
 
+
 def calcCrossVal(x, y):
     total = 0
     total += centres[x][y]
@@ -93,29 +89,3 @@ def calcCrossVal(x, y):
     if y > 0:
         total += centres[x][y - 1]
     return total
-
-# Implement me!
-def strategy(pid, board):
-    return [(0, 0), (0, 0), (0, 0)]
-
-# A random strategy to use in your game.
-def random_strategy(pid, board):
-    return [
-        (random.randint(0, 9), random.randint(0, 9)), 
-        (random.randint(0, 9), random.randint(0, 9)), 
-        (random.randint(0, 9), random.randint(0, 9)), 
-        ]
-
-# Edit me!
-def get_strategies():
-    """
-    Returns a list of strategy functions to use in a game.
-
-    In the local tester, all of the strategies will be used as separate players in the game.
-    Results will be printed out in the order of the list.
-
-    In the official grader, only the first element of the list will be used as your strategy. 
-    """
-    strategies = [greedy, random_strategy, random_strategy, random_strategy, random_strategy]
-
-    return strategies
