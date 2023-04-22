45e43575dff73cd229ee1469879a3545b7e9d384
Advay Patil <advaypatil27@gmail.com>
2023-04-21 20:01:26 -0700

    add the challenge

--- auctionhouse/auctionhouse/advay.py
+++ auctionhouse/auctionhouse/advay.py
@@ -25,3 +25,13 @@ def advay2(wallet, history):
     valid_amount = round(total * avgPerc)
 
     return 0 if valid_amount >= wallet else valid_amount + 1
+
+def ElDesafio(wallet, history):
+    if history[-1] == 0 and len(history) < 5:
+        return 1
+
+    sum_of_wallet = sum(list(map(lambda x: x[0], history)))
+    valid_amount = sum_of_wallet * 0.50
+
+    return 1 if valid_amount > wallet else valid_amount + 1
+
