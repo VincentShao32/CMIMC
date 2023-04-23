db488ad721d889ad630c03bead025ea2ca26f369
Advay Patil <advaypatil27@gmail.com>
2023-04-22 21:41:00 -0700

    random shit (ignore)

--- auctionhouse/auctionhouse/lastPlusOne.py
+++ auctionhouse/auctionhouse/lastPlusOne.py
@@ -11,12 +11,22 @@ def lastPlusOne(wallet, history):
 def lastPlusOneVaried(wallet, history):
     if len(history) == 0:
         return 0
-    if history[-1][0] + 1 > wallet * logistic_function(len(history)):
+    if history[-1][0] + 1 > wallet * get_percentage(len(history)):
         return 0
 
     return int(history[-1][0] + 1)
     
 
+def get_percentage(length):
+    if length < 3:
+        return 0.3
+    elif length < 6:
+        return 0.5
+    elif length < 9:
+        return 0.6
+    else:
+        return 0.7
+
 def logistic_function(x):
-    amount = 100 / (1 + math.exp(-0.2 * x))
+    amount = 100 / (1 + math.exp(-0.2 * (x-3)))
     return (100 - amount) / 100
--- auctionhouse/auctionhouse/strategy.py
+++ auctionhouse/auctionhouse/strategy.py
@@ -2,180 +2,179 @@
 Edit this file! This is the file you will submit.
 """
 
-import random
-from linear import linear
-from smart import smart
-from predatorCounter import predatorCounter
-from megamath import megamath
-from advay import *
-from fiftyFifty import fiftyFifty
-from lose import loseOpp
-from loseUs import loseUs
-from smart import smart
-from linear import linear
-from constant import constant
-from variation import variation
-from megamathCounter import randomShit
-from megamind import megamind
-from lastPlusOne import lastPlusOne, lastPlusOneVaried
+# import random
+# from linear import linear
+# from smart import smart
+# from predatorCounter import predatorCounter
+# from megamath import megamath
+# from advay import *
+# from fiftyFifty import fiftyFifty
+# from lose import loseOpp
+# from loseUs import loseUs
+# from smart import smart
+# from linear import linear
+# from megamathCounter import randomShit
+# from lastPlusOne import lastPlusOne, lastPlusOneVaried
+# from globalAbuser import counterOneMax,  counterOneMin
 
 # Implement me!
 # 2 example strategies to use in your tournament.
 
-from statistics import stdev, median, mode
-import math
+
+# from statistics import stdev, median, mode
+# import math
 
 
-def f(x):
-    return -12 * x + 100
+# def f(x):
+#     return -12 * x + 100
 
 
-def standardDev(points):
-    if len(points) == 1:
-        return points[0]
-    return stdev(points)
+# def standardDev(points):
+#     if len(points) == 1:
+#         return points[0]
+#     return stdev(points)
 
 
-def roundNum(num):
-    return round(num * 100) / 100
+# def roundNum(num):
+#     return round(num * 100) / 100
 
 
-def constantValue(history):
-    vals = [i[0] for i in history]
-    mid = median(vals)
-    if mid == mode(vals):
-        return True, mid
-    return False, -1
+# def constantValue(history):
+#     vals = [i[0] for i in history]
+#     mid = median(vals)
+#     if mid == mode(vals):
+#         return True, mid
+#     return False, -1
 
 
-def megamath(wallet, history):
-    oppSum = (100 - sum(i[0] for i in history))
-    if len(history) == 0:
-        return 0
+# def megamath(wallet, history):
+#     oppSum = (100 - sum(i[0] for i in history))
+#     if len(history) == 0:
+#         return 0
 
-    constant, val = constantValue(history)
-    if constant and val + 1 < wallet * 0.5:
-        return val + 1
+#     constant, val = constantValue(history)
+#     if constant and val + 1 < wallet * 0.5:
+#         return val + 1
 
-    total = 100
-    points1 = [history[0][0] / total]
-    total -= history[0][0]
-    for i in history[1:]:
-        if i[0] < 2:
-            continue
-        points1.append(roundNum(i[0] / total))
-        total -= i[0]
-    oppFactor = median(j for j in points1)
+#     total = 100
+#     points1 = [history[0][0] / total]
+#     total -= history[0][0]
+#     for i in history[1:]:
+#         if i[0] < 2:
+#             continue
+#         points1.append(roundNum(i[0] / total))
+#         total -= i[0]
+#     oppFactor = median(j for j in points1)
 
-    points2 = [history[0][0] / 100]
-    for i, j in enumerate(history[1:]):
-        if j[0] < 2:
-            continue
-        points2.append(roundNum(j[0] / f(i - 1)))
-    selfFactor = median(j for j in points2)
-    useFactor = selfFactor if standardDev(
-        points2) < standardDev(points1) else oppFactor
+#     points2 = [history[0][0] / 100]
+#     for i, j in enumerate(history[1:]):
+#         if j[0] < 2:
+#             continue
+#         points2.append(roundNum(j[0] / f(i - 1)))
+#     selfFactor = median(j for j in points2)
+#     useFactor = selfFactor if standardDev(
+#         points2) < standardDev(points1) else oppFactor
 
-    # useFactor = oppFactor
-    if useFactor == selfFactor:
-        return min(wallet, int(wallet * (useFactor + 0.125)))
+#     # useFactor = oppFactor
+#     if useFactor == selfFactor:
+#         return min(wallet, int(wallet * (useFactor + 0.125)))
 
-    if (oppSum * (useFactor + 0.125) > wallet * 0.5):
-        return 0
-    return int(oppSum * (useFactor + 0.125))
-
-
-def mCalc(n, points):
-    prodSum = 0
-    xSum = 0
-    ySum = 0
-    xSquaredSum = 0
-    for x, point in enumerate(points):
-        prodSum += x * point
-        xSum += x
-        ySum += point
-        xSquaredSum += x ** 2
-
-    return roundNum((n * prodSum - xSum * ySum) / (n * xSquaredSum - xSum ** 2)), xSum, ySum
+#     if (oppSum * (useFactor + 0.125) > wallet * 0.5):
+#         return 0
+#     return int(oppSum * (useFactor + 0.125))
 
 
-def bCalc(m, n, xSum, ySum):
-    return roundNum((ySum - m * xSum) / n)
+# def mCalc(n, points):
+#     prodSum = 0
+#     xSum = 0
+#     ySum = 0
+#     xSquaredSum = 0
+#     for x, point in enumerate(points):
+#         prodSum += x * point
+#         xSum += x
+#         ySum += point
+#         xSquaredSum += x ** 2
+
+#     return roundNum((n * prodSum - xSum * ySum) / (n * xSquaredSum - xSum ** 2)), xSum, ySum
 
 
-def linRegression(points):
-    m, xSum, ySum = mCalc(len(points), points)
-    b = bCalc(m, len(points), xSum, ySum)
-    return m, b
+# def bCalc(m, n, xSum, ySum):
+#     return roundNum((ySum - m * xSum) / n)
 
 
-def roundNum(num):
-    return round(num * 100) / 100
+# def linRegression(points):
+#     m, xSum, ySum = mCalc(len(points), points)
+#     b = bCalc(m, len(points), xSum, ySum)
+#     return m, b
 
 
-def g(m, b, x):
-    return m * x + b
+# def roundNum(num):
+#     return round(num * 100) / 100
 
 
-def RMSE(m, b, points):
-    errSum = 0
-    for i, point in enumerate(points):
-        errSum += (point - g(m, b, i)) ** 2
-    return math.sqrt((1 / len(points)) * errSum)
+# def g(m, b, x):
+#     return m * x + b
 
 
-def constantValue(history):
-    vals = [i[0] for i in history]
-    mid = median(vals)
-    if mid == mode(vals):
-        return True, mid
-    return False, -1
+# def RMSE(m, b, points):
+#     errSum = 0
+#     for i, point in enumerate(points):
+#         errSum += (point - g(m, b, i)) ** 2
+#     return math.sqrt((1 / len(points)) * errSum)
 
 
-def megamind(wallet, history):
-    oppSum = (100 - sum(i[0] for i in history))
-    if len(history) == 0:
-        return 2
-    constant, val = constantValue(history)
-    if constant and val + 1 < wallet * 0.6:
-        return int(val + 1)
-    oppBets = [history[0][0] / 100]
-    total = 100 - history[0][0]
-    for i in history[1:]:
-        if i[0] < 2:
-            continue
-        oppBets.append(roundNum(i[0] / total))
-        total -= i[0]
-    if len(oppBets) >= 2:
-        oppm, oppb = linRegression(oppBets)
+# def constantValue(history):
+#     vals = [i[0] for i in history]
+#     mid = median(vals)
+#     if mid == mode(vals):
+#         return True, mid
+#     return False, -1
 
-        oppNext = g(oppm, oppb, len(oppBets))
-    else:
-        oppNext = 0.2
 
-    # selfBets = [history[0][0] / 100]
-    # for i, j in enumerate(history[1:]):
-    #     if j[0] < 1:
-    #         continue
-    #     selfBets.append(roundNum(j[0] / f(i - 1)))
+# def megamind(wallet, history):
+#     oppSum = (100 - sum(i[0] for i in history))
+#     if len(history) == 0:
+#         return 2
+#     constant, val = constantValue(history)
+#     if constant and val + 1 < wallet * 0.6:
+#         return int(val + 1)
+#     oppBets = [history[0][0] / 100]
+#     total = 100 - history[0][0]
+#     for i in history[1:]:
+#         if i[0] < 2:
+#             continue
+#         oppBets.append(roundNum(i[0] / total))
+#         total -= i[0]
+#     if len(oppBets) >= 2:
+#         oppm, oppb = linRegression(oppBets)
 
-    # if len(selfBets) >= 2:
-    #     selfm, selfb = linRegression(selfBets)
-    #     # for i in history:
-    #     #     print(i[0])
-    #     selfNext = g(selfm, selfb, len(selfBets))
-    # else:
-    #     selfNext = 0.2
+#         oppNext = g(oppm, oppb, len(oppBets))
+#     else:
+#         oppNext = 0.2
 
-    # if selfNext == oppNext:
-    #     return int(oppSum * selfNext)
-    # use = oppNext if RMSE(oppm, oppb, oppBets) < RMSE(
-    #     selfm, selfb, selfBets) else selfNext
+#     # selfBets = [history[0][0] / 100]
+#     # for i, j in enumerate(history[1:]):
+#     #     if j[0] < 1:
+#     #         continue
+#     #     selfBets.append(roundNum(j[0] / f(i - 1)))
 
-    if oppSum * oppNext > wallet * 0.6:
-        return 0
+#     # if len(selfBets) >= 2:
+#     #     selfm, selfb = linRegression(selfBets)
+#     #     # for i in history:
+#     #     #     print(i[0])
+#     #     selfNext = g(selfm, selfb, len(selfBets))
+#     # else:
+#     #     selfNext = 0.2
 
-    return int(wallet * oppNext)
+#     # if selfNext == oppNext:
+#     #     return int(oppSum * selfNext)
+#     # use = oppNext if RMSE(oppm, oppb, oppBets) < RMSE(
+#     #     selfm, selfb, selfBets) else selfNext
+
+#     if oppSum * oppNext > wallet * 0.6:
+#         return 0
+
+#     return int(wallet * oppNext)
 
 
 def lastPlusOne(wallet, history):
@@ -188,25 +187,25 @@ def lastPlusOne(wallet, history):
     return int(history[-1][0] + 1)
 
 
-def gambler(wallet, history):
-    return random.randint(0, wallet)
+# def gambler(wallet, history):
+#     return random.randint(0, wallet)
 
 
-def villain(wallet, history):
-    return max(wallet - 1, 0)
+# def villain(wallet, history):
+#     return max(wallet - 1, 0)
 
 
-def randomShit(wallet, history):
+# def randomShit(wallet, history):
 
-    if len(history) < 4:
-        return 1
+#     if len(history) < 4:
+#         return 1
 
-    oppSum = 100 - sum(list(map(lambda x: x[0], history)))
+#     oppSum = 100 - sum(list(map(lambda x: x[0], history)))
 
-    if oppSum * 0.6 > wallet:
-        return 0
+#     if oppSum * 0.6 > wallet:
+#         return 0
 
-    return min(oppSum * 0.5 + 1, wallet)
+#     return min(oppSum * 0.5 + 1, wallet)
 
 
 def get_strategies():
@@ -219,7 +218,7 @@ def get_strategies():
 
     In the official grader, only the first element of the list will be used as your strategy. 
     """
-    strategies = ([lastPlusOne, lastPlusOneVaried] + [linear] * 10)
+    strategies = [lastPlusOne] * 5 
     # strategies = ([gambler] + [megamath])
     # strategies = ([megamath, fiftyFifty])
     return strategies
